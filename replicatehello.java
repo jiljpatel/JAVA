09ac4222292f0f0113bf0b62d164f457328a6b23
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-01-12 14:19:22 +0530

    create replicatehello.java

---
+++ replicatehello.java
@@ -0,0 +1,13 @@
+//Hello world replicated Programe .
+
+class hello
+{
+   public static void main(string[] args)
+{
+system.out.println("Enter your Name :");
+name v = new name();
+system.out.println("hello, "+v);
+
+
+}
+}
