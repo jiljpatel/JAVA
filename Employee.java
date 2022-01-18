40d59d4a79b779b0915842697daeecca081f7fb4
jiljpatel <94222083+jiljpatel@users.noreply.github.com>
2022-01-18 11:22:33 +0530

    Create employee.java

---
+++ Employee.java
@@ -0,0 +1,31 @@
+
+public class EmployeesDetails {
+	public static void main(String[] args) {
+		
+	Name();   Yearofjoining();  Address();
+	
+	
+
+	}
+	public static void  Name()
+	{
+
+		System.out.println("Robert");
+		System.out.println("Sam");
+		System.out.println("John");
+	}
+	public static void Yearofjoining()
+	{
+		System.out.println("1994");
+		System.out.println("2000");
+		System.out.println("1999");
+	}
+	public static void Address()
+	{
+		System.out.println("64C -WallStreet");
+		System.out.println("68D -WallStreet");
+		System.out.println("26B -WallStreet");
+		
+	}
+
+}
